0c433370ea7974b813eb5feb596ea4debda0b04a
09santi96 <09santio96@gmail.com>
2023-06-23 17:24:42 -0300

    servicios de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { BuclesComponent } from './bucles/bucles.component';
+import { SwichComponent } from './swich/swich.component';
+import { Formulario2Component } from './formulario2/formulario2.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Formulario3Component } from './formulario3/formulario3.component';
+import { ChildrenComponent } from './children/children.component';
+import { AddMessagesComponent } from './add-messages/add-messages.component';
+import { ListMessagesComponent } from './list-messages/list-messages.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioComponent,
-    BuclesComponent
+    BuclesComponent,
+    SwichComponent,
+    Formulario2Component,
+    Formulario3Component,
+    ChildrenComponent,
+    AddMessagesComponent,
+    ListMessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/swich/swich.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swich',
+  templateUrl: './swich.component.html',
+  styleUrls: ['./swich.component.css']
+})
+export class SwichComponent {
+  dia?: string;
+
+}
